069ff66962470488543deb377b07d1f8c38905db
jonathan Day <jday.day03@gmail.com>
2018-09-02 20:38:56 -0700

    Fixed init function for bitmex class

--- Bitmex.py
+++ Bitmex.py
@@ -1,8 +1,10 @@
 from market import market
 
 # a controller for ONE bitmex connection. This is a basic formula for how it should look. 
-#Feel free to message me about the design.
+# Feel free to message me about the design.
 class Bitmex(market):
+    # You will store the market API object in bitmex variable in init function
+    bitmex = None
     def limitSell(self, price,currency, asset):
         pass
 
@@ -23,7 +25,10 @@ class Bitmex(market):
     
     # use this function to handle connecting to the market (this function is the constructor)
     # You should definitely add parameters to this, probably the api key info
-    def __init__(self):
-
+    def __init__(self, priceMargin, maximum, limitThreshold):
+        # The super function runs the constructor on the market class that this class inherits from. In other words,
+        # done mess with it or the parameters I put in this init function
+        super(Bitmex, self).__init__(priceMargin, maximum, limitThreshold)
+        pass;
 
 # inherit market
