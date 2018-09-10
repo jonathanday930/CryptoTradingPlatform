f26f590d6a873069a9b5da3ecfaf13fb46a34907
alexPanch0 <alexmckee!1>
2018-09-09 18:36:21 -0700

    my bad

--- Bitmex.py
+++ Bitmex.py
@@ -7,31 +7,31 @@ from market import market
 # a controller for ONE bitmex connection. This is a basic formula for how it should look.
 class Bitmex(market):
     bitmex = None
-    def limitSell(self, price, currency, asset, orderId, orderQuantity):
+    def limitSell(self, price, asset, currency, orderId, orderQuantity):
         # TODO: figure out quantity params
         orderQuantity = orderQuantity * -1
-        self.bitmex.Order.Order_new(symbol=currency + asset, orderQty=orderQuantity, price=price, ordType="Limit").result()
+        self.bitmex.Order.Order_new(symbol=asset + currency, orderQty=orderQuantity, price=price, ordType="Limit").result()
 
     def getAmountOfItem(self, coin=None):
         return self.bitmex.User.User_getMargin().result()[0]['availableMargin']/self.btcToSatoshi
 
     # TODO: we need to get the current amount of the symbol that we already have so that we can sell it all and then place the short with the negative orderQuantity
-    def limitShortStart(self, price, currency, asset):
+    def limitShortStart(self, price, asset, currency):
         orderQuantity = 10
         orderQuantity = orderQuantity * -1
-        self.bitmex.Order.Order_new(symbol=currency + asset, orderQty=orderQuantity, price=price, ordType="Limit").result()
+        self.bitmex.Order.Order_new(symbol=asset + currency, orderQty=orderQuantity, price=price, ordType="Limit").result()
         pass
 
     def limitShortEnd(self, price, currency, asset):
         pass
 
-    def marketBuy(self, orderQuantity, currency, asset):
-        self.bitmex.Order.Order_new(symbol=currency+asset, orderQty=orderQuantity, ordType="Market").result()
+    def marketBuy(self, orderQuantity, asset, currency):
+        self.bitmex.Order.Order_new(symbol=asset+currency, orderQty=orderQuantity, ordType="Market").result()
         pass
 
-    def limitBuy(self, price, currency, asset, orderQuantity, orderId):
+    def limitBuy(self, price, asset, currency, orderQuantity, orderId):
         if orderId == None:
-            result = self.bitmex.Order.Order_new(symbol=currency + asset, orderQty=orderQuantity, ordType="Limit", price=price).result()
+            result = self.bitmex.Order.Order_new(symbol=asset + currency, orderQty=orderQuantity, ordType="Limit", price=price).result()
             tradeInfo = result[0]
             for key, value in tradeInfo.items():
                 if key == "orderID":
@@ -43,7 +43,7 @@ class Bitmex(market):
 
         return None
 
-    def getCurrentPrice(self, currency, asset):
+    def getCurrentPrice(self, asset, currency):
         startTime = datetime.datetime.now() - datetime.timedelta(minutes=1)
         trades = self.bitmex.Trade.Trade_get(symbol=asset+currency, startTime=startTime).result()
         sum = 0
@@ -53,17 +53,17 @@ class Bitmex(market):
             volume = volume + trade['size']
         return sum / volume
 
-    def closeLimitOrders(self, currency, asset):
+    def closeLimitOrders(self, asset, currency):
         # client.Order.Order_cancel(orderID='').result()
         self.bitmex.Order.Order_cancelAll().result()
         pass
 
-    def get_orders(self, currency, asset):
+    def get_orders(self, asset, currency):
         # .open_orders() doesn't seem to work
         # return self.bitmex.open_orders()
 
         ### get your orders
-        orders = self.bitmex.Order.Order_getOrders(symbol=currency+asset, reverse=True).result()
+        orders = self.bitmex.Order.Order_getOrders(symbol=asset+currency, reverse=True).result()
         orderList = orders[0]
         # TODO: your decision on how we keep the ledger for all of our trades, but this will print all of our trades and their status nicely if you wanna uncomment it
         # for i in range(len(orderList)):
--- controller.py
+++ controller.py
@@ -23,17 +23,12 @@ class controller:
         self.marketControllers[name] = market
 
     def createOrder(self, email):
-        self.marketControllers['bitmex'].followingLimitOrder(email.parameters[typeSubjectNumber],
-                                                                                       email.parameters[
-                                                                                           currencySubjectNumber],
-                                                                                       email.parameters[
-                                                                                           assetSubjectNumber])
-        # self.marketControllers['bitmex']
-        # Bitmex = self.marketControllers[0]
-        # self.bitmex.followingLimitOrder(
-        #     email.parameters[typeSubjectNumber],
-        #     email.parameters[
-        #         currencySubjectNumber],
-        #     email.parameters[
-        #         assetSubjectNumber])
+        # self.marketControllers['bitmex'].followingLimitOrder(email.parameters[typeSubjectNumber],
+        #                                                                                email.parameters[
+        #                                                                                    currencySubjectNumber],
+        #                                                                                email.parameters[
+        #                                                                                    assetSubjectNumber])
+
+        self.marketControllers['bitmex'].marketBuy(20, email.parameters[currencySubjectNumber],
+                                                             email.parameters[assetSubjectNumber])
 
