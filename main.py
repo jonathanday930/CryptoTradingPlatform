38e1e2a7401c933c114a2582be17729bfe6b72be
Tyler Ettner <tylerettner14@gmail.com>
2018-09-21 15:37:21 -0700

    Refactored to BinanceTrader

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 import logger
 from Bitmex import Bitmex
+import BinanceTrader
 from gmailHandler import gmailHandler
 from controller import controller
 
@@ -20,19 +21,21 @@ def main():
     #         email.print()
     #         count = count + 1
 
-    # controller tests
-    trader = controller(gmailHandler('credentials.json'), .1, .1, .1)
-    # trader.addMarket(Bitmex("4Utv4iqL31w88lLvRXN7qDtq", "lmoptxdUE8wFcn9GDdJRaecn_XfXDwfJ8_B14Bk3FOIK5Z9q",False,"test"),'BITMEX')
-    trader.importAPIKeys()
+    # # controller tests
+    # trader = controller(gmailHandler('credentials.json'), .1, .1, .1)
+    # # trader.addMarket(Bitmex("4Utv4iqL31w88lLvRXN7qDtq", "lmoptxdUE8wFcn9GDdJRaecn_XfXDwfJ8_B14Bk3FOIK5Z9q",False,"test"),'BITMEX')
+    # trader.importAPIKeys()
+    #
+    # trader.addMarket(Bitmex(.2, .11, .1, "B3aMhRrNhewW1SfHYT3M4h", "gQLdIXBUXar7PoDUND2DKYsLfAfMh0isCCmPQJjaDUxO0pdV"),
+    #                  'BITMEX')
+    #
+    # while True:
+    #     try:
+    #         trader.run()
+    #     except Exception as e:
+    #         logger.logError(e)
+    test=BinanceTrader("D82391RviO0eyFGm4QMK3EGTkd2D0p4viQesB8W4tVVpSo4nIUo0IDnVcCk65OGR","G1n85emNiXJNWdMWAE7HlwQ1FvDx7OSNE8ZN8hmqNgvj5pJMTuU0XVUjyRRa2G4d")
 
-    trader.addMarket(Bitmex(.2, .11, .1, "B3aMhRrNhewW1SfHYT3M4h", "gQLdIXBUXar7PoDUND2DKYsLfAfMh0isCCmPQJjaDUxO0pdV"),
-                     'BITMEX')
-
-    while True:
-        try:
-            trader.run()
-        except Exception as e:
-            logger.logError(e)
 
     # market = Bitmex(.1,.1,.1,"Bm23pmDAYgPq4JN-bbKipuq_", "gMH-WNVpS17cstY_0YOCe8kirlItoURrsYNCJKd6UhUjyoOp")
     # price = market.getMaxAmountToUse('ETH','U18')
